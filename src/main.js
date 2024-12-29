1a03986c043c83f9d374734acbc7d76f34f5fdd5
JuliaManzulya <ulamanzula@gmail.com>
2024-12-29 12:26:26 +0200

    Add file

--- src/main.js
+++ src/main.js
@@ -1,1 +1,21 @@
-console.log('Hey, this is my first JS program');
+// console.log('Hey, this is my first JS program');
+const enterNumber = +prompt('Введіть число');
+console.log(enterNumber);
+if (Number.isNaN(enterNumber)) {
+    console.log('Невірні дані');
+} else {
+    if (enterNumber % 3 === 0 && enterNumber % 5 === 0) {
+        console.log('FizzBizz');
+    } else 
+    if (enterNumber % 3 === 0) {
+      console.log('Fizz');  
+    } else 
+    if (enterNumber % 5 === 0) {
+    console.log('Bizz');
+    }
+    } 
+
+
+    
+
+  
