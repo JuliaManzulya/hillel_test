2bdbe7e1add8c692a6fb5102f77d43977089f0d1
JuliaManzulya <ulamanzula@gmail.com>
2024-12-29 15:40:23 +0200

    Add file

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 // console.log('Hey, this is my first JS program');
-const enterNumber = +prompt('Введіть число');
+/* const enterNumber = +prompt('Введіть число');
 console.log(enterNumber);
 if (Number.isNaN(enterNumber)) {
-    console.log('Невірні дані');
+    console.log('Некоректні дані');
 } else {
     if (enterNumber % 3 === 0 && enterNumber % 5 === 0) {
         console.log('FizzBizz');
@@ -14,8 +14,19 @@ if (Number.isNaN(enterNumber)) {
     console.log('Bizz');
     }
     } 
+    */
 
-
+// 2 завдання
+const enterYear = +prompt('Введіть рік')
+    console.log(enterYear);
+    if (Number.isNaN(enterYear)) {
+        console.log('Некоректні дані');
+    } else {
+        if ((enterYear % 4 === 0 && enterYear % 100 !== 0) || enterYear % 400 === 0) {
+           console.log('Високосний рік')
+    } else
+    if (enterYear % 4 !== 0 && enterYear % 400 !== 0)
+        console.log('Не високосний рік')
+}
     
-
-  
+    
